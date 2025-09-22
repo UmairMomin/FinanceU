df17a4bbde3d9364ab48438574dcf6170ddb161c
UmairMomin <umairmomin04@gmail.com>
2025-09-22 13:53:03 +0530

    updated cors issue

--- frontend/src/services/chatServices.js
+++ frontend/src/services/chatServices.js
@@ -28,6 +28,10 @@ const makeGeminiRequest = async (prompt, context = null, expert) => {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': ALLOWED_ORIGIN,
+        'Access-Control-Allow-Methods': 'GET, POST, OPTIONS',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+        'Access-Control-Allow-Credentials': 'true',
       },
       body: JSON.stringify(payload)
     });
